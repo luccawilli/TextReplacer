b58f849c03f7e6b4f2ece15390cf7234419aab68
luccawilli <lucca.willi@gmail.com>
2022-01-18 18:26:04 +0100

    Views hinzugefügt, Beginn Umstieg auf MultiView

---
+++ TextReplacer/Control/CustomTabHeader.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TextReplacer.Control {
+  /// <summary>
+  /// Interaction logic for CustomTabHeader.xaml
+  /// </summary>
+  public partial class CustomTabHeader : UserControl {
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(CustomTabHeader), new UIPropertyMetadata());
+    public String Text {
+      get { return (String)GetValue(TextProperty); }
+      set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageSourceProperty =
+        DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(CustomTabHeader), new UIPropertyMetadata());
+    public ImageSource ImageSource {
+      get { return (ImageSource)GetValue(ImageSourceProperty); }
+      set { SetValue(ImageSourceProperty, value); }
+    }
+
+
+    public static readonly DependencyProperty ImageWidthProperty =
+        DependencyProperty.Register("ImageWidth", typeof(Int32), typeof(CustomTabHeader), new UIPropertyMetadata(20));
+    public Int32 ImageWidth {
+      get { return (Int32)GetValue(ImageWidthProperty); }
+      set { SetValue(ImageWidthProperty, value); }
+    }
+
+    public CustomTabHeader() {
+      InitializeComponent();
+    }
+  }
+}
---
+++ TextReplacer/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TextReplacer.ViewModel {
+  public class MainViewModel /*: INotifyPropertyChanged*/ {
+
+    public MainViewModel() { }
+
+    public ObservableCollection<InfoMessage> InfoMessages { get; set; }
+
+    private String _replacementChars;
+    public String ReplacementChars {
+      get => _replacementChars;
+      set => _replacementChars = value;
+    }
+
+    //public event PropertyChangedEventHandler PropertyChanged;
+  }
+}
